081e85fa6309e510bd40ee31b4a29b05ec4a6f58
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-06-10 21:34:57 +0100

    functions start

---
+++ P08_FUNCTIONS_DEMO/locals.tf
@@ -0,0 +1,20 @@
+locals {
+
+  common_tags = {
+    company      = var.company
+    project      = "${var.company}-${var.project}"
+    billing_code = var.billing_code
+  }
+
+  s3_bucket_name = "globo-web-app-${random_integer.s3.result}"
+
+  website_content = {
+    website = "/website/index.html"
+    logo    = "/website/Globo_logo_Vert.png"
+  }
+}
+
+resource "random_integer" "s3" {
+  min = 10000
+  max = 99999
+}
